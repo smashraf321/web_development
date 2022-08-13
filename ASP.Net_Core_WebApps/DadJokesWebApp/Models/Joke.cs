c5a60b36ddba56e52ae4ea164d55801ab9c9c75a
ashrafSisLaptop <sma.muhammad.321@gmail.com>
2022-08-13 22:46:57 +0530

    Create a model for a joke

---
+++ ASP.Net_Core_WebApps/DadJokesWebApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace DadJokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
